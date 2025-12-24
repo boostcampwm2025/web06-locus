83b9b18638a0327cb375c721fff803f1edc45c18
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-12-24 17:11:19 +0900

    chore: Stroybook 설정 추가
    - Storybook 초기화 및 Vite 통합
    - Vitest 연동 설정

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,20 +1,28 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import base from '../../eslint.config.mjs';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default defineConfig(...base, {
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    extends: [
-        reactHooks.configs['recommended-latest'],
-        reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-        globals: { ...globals.browser },
-        parserOptions: {
-            projectService: true,
-            tsconfigRootDir: import.meta.dirname,
+export default defineConfig(
+    ...base,
+    {
+        files: ['**/*.{ts,tsx,js,jsx}'],
+        extends: [
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            globals: { ...globals.browser },
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
     },
-});
+    // Storybook 파일에 대한 ESLint 설정
+    ...storybook.configs['recommended'],
+);
