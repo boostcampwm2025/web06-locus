7a3a2715857d8d161e9b46d574c49c5e6605c314
beomsic <hhqjatjr@gmail.com>
2025-12-26 16:57:17 +0900

    chore: 데코레이터가 있는 empty class 허용하도록 eslint rule 추가

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -3,17 +3,21 @@ import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 
 export default defineConfig(...base, {
-    files: ['**/*.{ts,js}'],
-    languageOptions: {
-        globals: { ...globals.node, ...globals.jest },
-        parserOptions: {
-            projectService: true,
-            tsconfigRootDir: import.meta.dirname,
-        },
+  files: ['**/*.{ts,js}'],
+  languageOptions: {
+    globals: { ...globals.node, ...globals.jest },
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
     },
-    rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-floating-promises': 'warn',
-        '@typescript-eslint/no-unsafe-argument': 'warn',
-    },
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/no-extraneous-class': [
+      'error',
+      { allowWithDecorator: true },
+    ],
+  },
 });
