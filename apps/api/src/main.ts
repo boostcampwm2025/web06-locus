85ed2878dd4eec419aa9107bde0bd36ec2b4e646
beomsic <hhqjatjr@gmail.com>
2025-12-29 17:42:44 +0900

    refactor: ESM 임포트 확장자 제거 및 async 호출 개선
    - .js 확장자 제거
    - bootstrap() 앞에 void 연산자 추가 (floating promise 경고 해결)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module.js';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,4 +7,4 @@ async function bootstrap() {
 
   app.enableShutdownHooks();
 }
-bootstrap();
+void bootstrap();
