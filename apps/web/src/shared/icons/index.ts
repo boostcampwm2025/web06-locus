67a8cad79010e9bab5200fdf94e0950234f16549
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-12-30 14:21:17 +0900

    feat(icons): 로그인 관련 아이콘 컴포넌트 추가
    - GoogleIcon, NaverIcon, KakaoIcon 소셜 로그인 아이콘 추가
    - Logo 브랜드 로고 컴포넌트 추가
    - IconProps 타입을 사용한 일관된 인터페이스
    - index.ts에 export 추가

--- apps/web/src/shared/icons/index.ts
+++ apps/web/src/shared/icons/index.ts
@@ -1,2 +1,6 @@
 export { default as ErrorIcon } from './ErrorIcon';
 export { default as RefreshIcon } from './RefreshIcon';
+export { default as GoogleIcon } from './GoogleIcon';
+export { default as NaverIcon } from './NaverIcon';
+export { default as KakaoIcon } from './KakaoIcon';
+export { default as Logo } from './Logo';
