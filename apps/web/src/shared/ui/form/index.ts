f024108cd18d648898d25fe94e336ebd2ad297e9
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2026-01-06 14:49:44 +0900

    feat: 폼 공통 컴포넌트 추가
    - FormSection: 폼 섹션 래퍼 컴포넌트 (제목 + 컨텐츠)
    - TextAreaField: 다중 라인 텍스트 입력 필드 (FormSection 포함)
    - ImageUploadButton: 이미지 업로드 버튼 (FormSection 포함)
    - form.ts: 폼 관련 타입 정의 추가

---
+++ apps/web/src/shared/ui/form/index.ts
@@ -0,0 +1,3 @@
+export { default as FormSection } from './FormSection';
+export { default as TextAreaField } from './TextAreaField';
+export { default as ImageUploadButton } from './ImageUploadButton';
