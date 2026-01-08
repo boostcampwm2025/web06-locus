177460f8ebcaeb7046c92e15a030bebd1a5722e8
peanut990 <peanut1998@naver.com>
2026-01-08 16:11:27 +0900

    chore: 커스텀 예외 사용 예시 @example 적용

--- apps/api/src/common/exceptions/business.exception.ts
+++ apps/api/src/common/exceptions/business.exception.ts
@@ -2,13 +2,13 @@ import { HttpException } from '@nestjs/common';
 
 /**
  * 비즈니스 예외의 공통 베이스 클래스
-  커스텀 비즈니스 예외들은 전부 BusinessException 상속받아 사용한다.
-// 예)
-// export class UserNotFoundException extends BusinessException {
-//   constructor(userId: string) {
-//     super(404, 'USER_NOT_FOUND', 'User does not exist', { userId });
-//   }
-// }
+ * 커스텀 비즈니스 예외들은 전부 BusinessException 상속받아 사용한다.
+ * @example
+ * export class UserNotFoundException extends BusinessException {
+ *    constructor(userId: string) {
+ *      super(404, 'USER_NOT_FOUND', 'User does not exist', { userId });
+ *    }
+ *  }
  */
 export class BusinessException extends HttpException {
   constructor(
