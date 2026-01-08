0bbf570c79834e7182f9fe51494798c8081d4db7
vaaast-lake <kown9512@gmail.com>
2026-01-08 14:50:24 +0900

    fix: BaseBottomSheet의 children 받는 div 태그는 자식 태그의 레이아웃 역할만 하도록 수정

--- apps/web/src/shared/ui/bottomSheet/BaseBottomSheet.tsx
+++ apps/web/src/shared/ui/bottomSheet/BaseBottomSheet.tsx
@@ -75,10 +75,7 @@ export default function BaseBottomSheet({
             />
           </div>
         )}
-        <div
-          className="flex-1 flex flex-col min-h-0"
-          aria-label="바텀시트 자식"
-        >
+        <div className="flex-1 min-h-0" aria-label="바텀시트 자식">
           {children}
         </div>
       </div>
