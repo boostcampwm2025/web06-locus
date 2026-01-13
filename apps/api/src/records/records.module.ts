2f590b663d38275f455ed3c1458f3a2953a99aef
vaaast-lake <kown9512@gmail.com>
2026-01-13 23:09:49 +0900

    feat: record 모듈에 jwt 모듈 추가

--- apps/api/src/records/records.module.ts
+++ apps/api/src/records/records.module.ts
@@ -3,9 +3,10 @@ import { RecordsController } from './records.controller';
 import { RecordsService } from './records.service';
 import { ReverseGeocodingService } from './services/reverse-geocoding.service';
 import { PrismaModule } from '@/prisma/prisma.module';
+import { JwtModule } from '@/jwt/jwt.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule],
   controllers: [RecordsController],
   providers: [RecordsService, ReverseGeocodingService],
   exports: [RecordsService],
