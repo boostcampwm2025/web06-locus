da030d82d9b55dd706eba3f17de27cccd4600ab4
beomsic <hhqjatjr@gmail.com>
2026-01-14 14:59:10 +0900

    feat: dto에 class-validator 를 적용

--- apps/api/src/record/dto/create-record.dto.ts
+++ apps/api/src/record/dto/create-record.dto.ts
@@ -1,6 +1,21 @@
+import { IsString, IsNotEmpty, IsNumber, Min, Max } from 'class-validator';
+
 export class CreateRecordDto {
+  @IsString()
+  @IsNotEmpty({ message: '제목은 필수 항목입니다.' })
   title: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '내용은 필수 항목입니다.' })
   content: string;
+
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
   latitude: number;
+
+  @IsNumber()
+  @Min(-180)
+  @Max(180)
   longitude: number;
 }
