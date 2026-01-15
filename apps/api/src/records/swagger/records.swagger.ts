c2911a0c165c535e42345ffade9132ec07a9f936
vaaast-lake <kown9512@gmail.com>
2026-01-15 20:33:05 +0900

    refactor: createRecord 컨트롤러 스웨커 데코레이터 분리

---
+++ apps/api/src/records/swagger/records.swagger.ts
@@ -0,0 +1,62 @@
+import { applyDecorators, HttpStatus } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiBearerAuth,
+  ApiConsumes,
+  ApiExtraModels,
+} from '@nestjs/swagger';
+import {
+  ApiSuccessResponse,
+  ApiFailResponse,
+  ApiErrorResponse,
+} from '@/common/utils/swagger-response.helper';
+import { RecordResponseDto } from '../dto/record-response.dto';
+import { RecordErrorCode } from '../constants/error-codes';
+
+export const CreateRecordSwagger = () =>
+  applyDecorators(
+    ApiExtraModels(RecordResponseDto),
+    ApiBearerAuth(),
+    ApiConsumes('multipart/form-data'),
+    ApiOperation({
+      summary: '기록 생성',
+      description:
+        '새로운 위치 기반 기록을 생성합니다. 이미지는 선택 사항이며 최대 5개까지 업로드 가능합니다.',
+    }),
+    ApiSuccessResponse(RecordResponseDto, HttpStatus.CREATED),
+    ApiFailResponse(HttpStatus.BAD_REQUEST, [
+      {
+        code: RecordErrorCode.TITLE_MISSING,
+        message: '제목이 누락되었습니다.',
+      },
+      {
+        code: RecordErrorCode.TITLE_TOO_LONG,
+        message: '제목이 너무 깁니다. (최대 100자)',
+      },
+      {
+        code: RecordErrorCode.LOCATION_MISSING,
+        message: '위치 정보가 누락되었습니다.',
+      },
+      {
+        code: RecordErrorCode.INVALID_IMAGE_FORMAT,
+        message: '지원하지 않는 이미지 형식입니다.',
+      },
+      {
+        code: RecordErrorCode.IMAGE_SIZE_EXCEEDED,
+        message: '이미지 파일 크기가 초과되었습니다. (최대 2MB)',
+      },
+      {
+        code: RecordErrorCode.TOO_MANY_IMAGES,
+        message: '이미지 개수가 초과되었습니다. (최대 5개)',
+      },
+      {
+        code: RecordErrorCode.INVALID_JSON_FORMAT,
+        message: 'JSON 형식이 올바르지 않습니다.',
+      },
+    ]),
+    ApiFailResponse(HttpStatus.UNAUTHORIZED, {
+      code: 'AUTH_TOKEN_MISSING',
+      message: '인증 토큰이 없거나 유효하지 않습니다.',
+    }),
+    ApiErrorResponse(),
+  );
