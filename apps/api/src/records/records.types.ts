6c723110fb78f86b87dccf15c9561aaa14577200
vaaast-lake <kown9512@gmail.com>
2026-01-20 14:15:59 +0900

    feat: 이미지 모델 타입 추가

--- apps/api/src/records/records.types.ts
+++ apps/api/src/records/records.types.ts
@@ -13,6 +13,27 @@ export interface RecordModel {
   updatedAt: Date;
 }
 
+export interface ImageModel {
+  publicId: string;
+  order: number;
+  thumbnailUrl: string;
+  thumbnailWidth: number;
+  thumbnailHeight: number;
+  thumbnailSize: number;
+  mediumUrl: string;
+  mediumWidth: number;
+  mediumHeight: number;
+  mediumSize: number;
+  originalUrl: string;
+  originalWidth: number;
+  originalHeight: number;
+  originalSize: number;
+}
+
+export interface RecordWithImages extends RecordModel {
+  images: ImageModel[];
+}
+
 export interface LocationInfo {
   name: string | null;
   address: string | null;
