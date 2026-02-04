c65c991a40d06a1e3524dfdb166b27d2b27e19dc
ariimo <159447466+ariimo@users.noreply.github.com>
2026-02-04 15:21:53 +0900

    chore: set duck module

--- apps/api/src/duck/duck.module.ts
+++ apps/api/src/duck/duck.module.ts
@@ -4,11 +4,13 @@ import { DuckController } from './duck.controller';
 import { DuckService } from './duck.service';
 import { JwtModule } from '@/jwt/jwt.module';
 import { DuckConsumer } from './duck.consumer';
+import { NcpAiService } from './ncp-ai.service';
+import { UsersModule } from '@/users/users.module';
 
 @Module({
-  imports: [RecordsModule, JwtModule],
+  imports: [RecordsModule, JwtModule, UsersModule],
   controllers: [DuckController, DuckConsumer],
-  providers: [DuckService],
+  providers: [DuckService, NcpAiService],
   exports: [DuckService],
 })
 export class DuckModule {}
